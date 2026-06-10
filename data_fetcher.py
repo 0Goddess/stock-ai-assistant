dbabd27272b3eb231eb0ecad981f53a17772c739
0Goddess <52619254+0Goddess@users.noreply.github.com>
2026-06-10 08:10:59 +0800

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -197,17 +197,13 @@ def get_stock_price_data(stock_id):
     if df.empty:
         return None
 
-    df = df.dropna(
-        subset=[
-            "Close",
-            "High",
-            "Low",
-            "Volume"
-        ]
-    )
+try:
+    df = df.dropna()
+except Exception as e:
+    print("清除空值錯誤:", e)
 
-    if df.empty:
-        return None
+if df.empty:
+    return None
 
     close_series = df["Close"]
 
